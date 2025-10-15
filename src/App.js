932443d905d939c6c36a941860e951dcd45eef0b
Jack <j.griffithsrico@gmail.com>
2025-10-15 13:30:20 +0100

    Create lines and tiles from words

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import "./styles/styles.scss";
 
+const DAILY_WORDS = 8;
+
 export default function App()  {
   return (
-    <Grid solutions={["dog", "water", "food"]}/>
+    <Grid solutions={getWords()}/>
   );
 };
 
@@ -21,7 +23,13 @@ function Line({solution, type}){
 function Grid({solutions}){
     const lines = [];
     for (let i = 0; i < solutions.length; i++){
-        lines.push(<Line solution={solutions[i]} type={i}/>)
+        lines.push(<Line key={i} solution={solutions[i]} type={i}/>)
     }
     return <div className='board'>{lines}</div>
+}
+
+const getWords = () =>{
+    let words = ["animal", "lawnmower", "ambulance"];
+    // for (let i = 0; i < DAILY_WORDS; i++) words.push(await fetch(`https://api.datamuse.com/words?ml=duck&sp=b*`));
+    return words;
 }
